d64f876839a06fcb89c9c8183fed84c499856f06
Keaton Robert Gallagher <keaton@keaton.local>
2019-12-18 15:32:09 -0700

    Changing every character in a string minus the last four

---
+++ MaskingChars.js
@@ -0,0 +1,10 @@
+/* When you use a credit card it only shows the last four digits the others show # make this string output only the last for characters and the rest show # */
+
+var chars = "4556364607935616";
+
+//Get the last four characters of the string
+console.log("Last four characters ", chars.substr(chars.length -4));
+
+// it basically replaces all characters where there's more than 4 characters afterwards. So when it get to 4 characters before the end of the string, the regex will fail and won't replace those characters. 
+var newString = chars.replace(/.(?=.{4,}$)/g, '#');
+console.log(newString)
